b8630cdc159131e2b93dae4eb7a4ca03a73f554c
slaymed <mohabedr34@gmail.com>
2024-07-11 18:01:41 +0100

    Removed /api prefix from endpoints

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     cors: { origin: process.env.CLIENT_APP },
   });
-  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ exceptionFactory, whitelist: true }));
   app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
   const configService = app.get(ConfigService);
